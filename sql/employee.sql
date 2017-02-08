612b684d4421890b9726c9811dfd18f23ba8447c
Hannes Ehrensperger <hannes.ehrensperger@helsana.ch>
2017-02-08 09:36:08 +0100

    Neu erstellt

---
+++ sql/employee.sql
@@ -0,0 +1,20 @@
+CREATE TABLE EMPLOYEE (
+  ID NUMBER(10) NOT NULL,
+  first_name VARCHAR2(20),
+  last_name VARCHAR2(20),
+  salary NUMBER(10),
+  PRIMARY KEY (id)
+);
+
+CREATE SEQUENCE employee_seq START WITH 1;
+
+CREATE OR REPLACE TRIGGER employee_bir
+BEFORE INSERT ON EMPLOYEE
+FOR EACH ROW
+  BEGIN
+    SELECT employee_seq.nextval
+    INTO :new.id
+    FROM dual;
+  END;
+
+COMMIT;
